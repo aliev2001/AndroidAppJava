d65ad62cb071508913ed36a64a6e421e1de7c890
Zangar <ali.zangar00@mail.ru>
2021-02-21 23:30:00 +0600

    init

---
+++ app/src/main/java/com/example/ass4/Day_main.java
@@ -0,0 +1,14 @@
+package com.example.ass4;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Day_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_main);
+    }
+}
---
+++ app/src/main/java/com/example/ass4/Utils/LetterImageView.java
@@ -0,0 +1,87 @@
+package com.example.ass4.Utils;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+
+public class LetterImageView extends AppCompatImageView {
+
+    private char mLetter;
+    private Paint mTextPaint;
+    private Paint mBackgroundPaint;
+    private int mTextColor = Color.WHITE;
+    private boolean isOval;
+
+    public LetterImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setColor(mTextColor);
+        mBackgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBackgroundPaint.setStyle(Paint.Style.FILL);
+    }
+
+    public char getLetter() {
+        return mLetter;
+    }
+
+    public void setLetter(char letter) {
+        mLetter = letter;
+        invalidate();
+    }
+
+    public int getTextColor() {
+        return mTextColor;
+    }
+
+    public void setTextColor(int textColor) {
+        mTextColor = textColor;
+        invalidate();
+    }
+
+    public void setOval(boolean oval) {
+        isOval = oval;
+    }
+
+    public boolean isOval() {
+        return isOval;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (getDrawable() == null) {
+
+            mTextPaint.setTextSize(canvas.getHeight() - getTextPadding() * 2);
+            if (isOval()) {
+                canvas.drawCircle(canvas.getWidth() / 2f, canvas.getHeight() / 2f, Math.min(canvas.getWidth(), canvas.getHeight()) / 2f,
+                        mBackgroundPaint);
+            } else {
+                canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), mBackgroundPaint);
+            }
+
+            Rect textBounds = new Rect();
+            mTextPaint.getTextBounds(String.valueOf(mLetter), 0, 1, textBounds);
+            float textWidth = mTextPaint.measureText(String.valueOf(mLetter));
+            float textHeight = textBounds.height();
+
+            canvas.drawText(String.valueOf(mLetter), canvas.getWidth() / 2f - textWidth / 2f,
+                    canvas.getHeight() / 2f + textHeight / 2f, mTextPaint);
+        }
+    }
+
+    private float getTextPadding() {
+
+        return 8 * getResources().getDisplayMetrics().density;
+    }
+}
